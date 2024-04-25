6654deeb10ea71c7fa82bf7984fd0e2ef95475d2
Vaibhav Mukheja <79351721+muk679@users.noreply.github.com>
2024-04-25 23:57:10 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print("hii")
+print("Hii..This is pyhton file..Showing in workflows under github actions")
+# Define two numbers
+num1 = 5
+num2 = 3
+
+# Calculate the sum
+sum_of_numbers = num1 + num2
+
+# Print the result
+print("Sum of", num1, "and", num2, "is:", sum_of_numbers)
